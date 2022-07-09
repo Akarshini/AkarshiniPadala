80aac7255b34c04adbdc472238ee3931064ead67
akarshini padala <akarshinipadala24@gmail.com>
2022-07-09 15:04:13 +0530

    all files are ready to upload

---
+++ src/to-do.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+class TODO extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            text: '',
+            items: []
+        }
+        this.handleChange =this.handleChange.bind(this)
+        this.handleSubmit =this.handleSubmit.bind(this)
+    }
+    handleChange(e){
+    this.setState({text: e.target.value})
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        if(this.state.text.length === 0) return
+
+        const newItem ={
+            text:this.state.text,
+            id: Date.now()
+        }
+        this.setState(state=>({
+            items: state.items.concat(newItem),
+            text:''
+    }))
+}
+      render(){
+        return (
+            <div>
+            <h1>TODO app</h1>
+            <TODOList todoitems={this.state.items}/>
+            <form onSubmit={this.handleSubmit}>
+            <label htmlFor="id">Input Item</label>
+           
+            <input id="id" onChange={this.handleChange} value={this.state.text}/>
+            <button>Add</button>
+        </form>
+        </div>
+            )
+    }
+}
+export default TODO
+
+class TODOList extends React.Component {
+    render(){
+        return (
+            <ul>
+            {   //props get data from parent within the same class
+                this.props.todoitems.map(item =>(
+                    <li keys={item.id}>{item.text}</li>
+                ))}
+                </ul>
+        )
+    }
+    
+    
+}
